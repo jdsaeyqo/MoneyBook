90e4b26ad85d7e37fadef84136d67adba681fc70
Sang <jdsaeyqi@naver.com>
2021-07-16 10:18:44 +0900

    Money Entity

---
+++ data/src/main/java/com/example/data/entity/MoneyEntity.kt
@@ -0,0 +1,19 @@
+package com.example.data.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class MoneyEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val checked : String,
+    val date : String,
+    val money : String,
+    val separation : String,
+    val use : String,
+    val description: String,
+    val hasCompleted : Boolean = false
+
+) {
+}
