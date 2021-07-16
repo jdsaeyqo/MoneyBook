b8fdf61e6b6cf2104eeaf555e754b5af62e51877
Sang <jdsaeyqi@naver.com>
2021-07-16 10:22:54 +0900

    Money 모델

---
+++ domain/src/main/java/com/example/domain/model/Money.kt
@@ -0,0 +1,12 @@
+package com.example.domain.model
+
+data class Money(
+    val id : Long,
+    val checked : String,
+    val date : String,
+    val money : String,
+    val separation : String,
+    val use : String,
+    val description: String,
+    val hasCompleted : Boolean = false
+)
