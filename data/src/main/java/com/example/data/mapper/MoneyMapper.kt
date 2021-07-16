039d70590ff0bc7970899fa0388f5728e9aae746
Sang <jdsaeyqi@naver.com>
2021-07-16 10:20:26 +0900

    Mapper

---
+++ data/src/main/java/com/example/data/mapper/MoneyMapper.kt
@@ -0,0 +1,33 @@
+package com.example.data.mapper
+
+import com.example.data.entity.MoneyEntity
+import com.example.domain.model.Money
+
+object MoneyMapper {
+
+    fun mapperToMoney(moneyEntity: MoneyEntity) : Money{
+        return Money(
+            id = moneyEntity.id,
+            checked = moneyEntity.checked,
+            date = moneyEntity.date,
+            money = moneyEntity.money,
+            separation = moneyEntity.separation,
+            use = moneyEntity.use,
+            description = moneyEntity.description,
+            hasCompleted = moneyEntity.hasCompleted
+        )
+    }
+
+    fun mapperToMoneyEntity(money: Money) : MoneyEntity{
+        return MoneyEntity(
+            id = money.id,
+            checked = money.checked,
+            date = money.date,
+            money = money.money,
+            separation = money.separation,
+            use = money.use,
+            description = money.description,
+            hasCompleted = money.hasCompleted,
+        )
+    }
+}
